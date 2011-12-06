57da9317cc45597e6e083c28e08f903f41fb9748
cash <cash.costello@gmail.com>
2011-12-05 20:15:45 -0500

    added support for filtering plugin releases by Elgg version

--- start.php
+++ start.php
@@ -50,29 +50,56 @@ function community_ws_plugin_check($plugins, $version) {
 			// this means the project access is not set to public
 			continue;
 		}
+
+		// sort by newer releases first
 		$newer_releases = elgg_get_entities(array(
 			'type' => 'object',
 			'subtype' => 'plugin_release',
 			'container_guid' => $project->getGUID(),
 			'created_time_lower' => $release->getTimeCreated() + 1,
 			'order_by' => 'e.time_created desc',
+			'limit' => 0,
 		));
 
 		if ($newer_releases) {
-			$new_release = $newer_releases[0];
-			$dl_link = get_config('wwwroot');
-			$dl_link .= "pg/plugins/download/{$new_release->getGUID()}";
-			
-			$info = new stdClass();
-			$info->plugin_id = $plugin_hash;
-			$info->plugin_name = $project->title;
-			$info->plugin_version = $new_release->version;
-			$info->plugin_url = $new_release->getURL();
-			$info->download_url = $dl_link;
-			$updated_plugins[] = $info;
+			// loop from the newest to oldest, checking elgg version requirements
+			$index = 0;
+			while (isset($newer_releases[$index])) {
+				$plugin_require = community_ws_extract_version($newer_releases[$index]->elgg_version);
+				$elgg_version = community_ws_extract_version($version);
+				if ($plugin_require <= $version) {
+					$new_release = $newer_releases[$index];
+					$dl_link = get_config('wwwroot');
+					$dl_link .= "pg/plugins/download/{$new_release->getGUID()}";
+
+					$info = new stdClass();
+					$info->plugin_id = $plugin_hash;
+					$info->plugin_name = $project->title;
+					$info->plugin_version = $new_release->version;
+					$info->plugin_url = $new_release->getURL();
+					$info->download_url = $dl_link;
+					$updated_plugins[] = $info;
+
+					break;
+				}
+				$index++;
+			}
 		}
 
 	}
 
 	return $updated_plugins;
+}
+
+/**
+ * Turn an Elgg version string into a float
+ * @param string $version
+ * @return float
+ */
+function community_ws_extract_version($version) {
+	$elgg_version_arr = explode('.', $version);
+	$elgg_major_version = (int)$elgg_version_arr[0];
+	$elgg_minor_version = (int)$elgg_version_arr[1];
+
+	return (float)"$elgg_major_version.$elgg_minor_version";
 }
